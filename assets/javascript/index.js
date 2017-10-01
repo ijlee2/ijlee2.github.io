c8d93872afef45a9e41e83cfc9ccb0c8fefb79db
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-30 22:33:58 -0500

    scrolling added

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
 
     // Navbar for mobile
     $(".button-collapse").sideNav();
+
+    // Scroll to anchor tags
+    $(".ijl-desktop-items").click(function() {
+        const section = $(this).attr("href");
+
+        const destination = $(section).offset().top;
+        const duration    = Math.trunc((destination - $(document).scrollTop()) / 1.25);
+        
+        $("html, body").animate({"scrollTop": destination}, duration);
+    });
 });
