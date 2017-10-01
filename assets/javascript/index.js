e64e939373b8e67f518e1030da469b7fd38944b4
ijlee2 <ijlee2@users.noreply.github.com>
2017-09-30 22:55:30 -0500

    sticky navbar added

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,16 +1,31 @@
 $(document).ready(function() {
-    // Dropdown menu
-    $(".dropdown-button").dropdown();
-
     // Navbar for mobile
     $(".button-collapse").sideNav();
 
     // Scroll to anchor tags
-    $(".ijl-desktop-items").click(function() {
-        const section = $(this).attr("href");
+    $(".brand-logo").click(function() {
+        // Remove highlight
+        $("#ijl-menu-desktop > *").removeClass("active green darken-2");
 
+        // Find where to go and how fast
+        const destination = 0;
+        const duration    = Math.trunc(Math.abs(destination - $(document).scrollTop()) / 1.25);
+        
+        $("html, body").animate({"scrollTop": destination}, duration);
+    });
+
+    $(".ijl-menu-items-desktop").click(function() {
+        // Find which menu item was clicked
+        const index = $(".ijl-menu-items-desktop").index(this);
+        
+        // Highlight the menu item
+        $("#ijl-menu-desktop > *").removeClass("active green darken-2");
+        $(`#ijl-menu-desktop > li:nth-of-type(${index + 1})`).addClass("active green darken-2");
+
+        // Find where to go and how fast
+        const section     = $(this).attr("href");
         const destination = $(section).offset().top;
-        const duration    = Math.trunc((destination - $(document).scrollTop()) / 1.25);
+        const duration    = Math.trunc(Math.abs(destination - $(document).scrollTop()) / 1.25);
         
         $("html, body").animate({"scrollTop": destination}, duration);
     });
