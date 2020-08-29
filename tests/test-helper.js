a0e8e29d6dc701832e219aa8700d47777908f31b
ijlee2 <ijlee2@users.noreply.github.com>
2020-08-29 09:12:42 -0500

    Deployed 33cb257

--- tests/test-helper.js
+++
@@ -1,8 +0,0 @@
-import Application from 'ijlee2.github.io/app';
-import config from 'ijlee2.github.io/config/environment';
-import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
-
-setApplication(Application.create(config.APP));
-
-start();
